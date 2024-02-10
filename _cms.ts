4e4c80000b49278a4c60053573d6a0093d64dc1d
Oscar Otero <oom@oscarotero.com>
2024-02-10 17:40:56 +0100

    removed decapCms

---
+++ _cms.ts
@@ -0,0 +1,31 @@
+import cms from "lume/deps/cms.ts";
+
+const app = cms();
+
+app
+  .upload("imaxes", "src:img")
+  .collection(
+    "posts",
+    "src:posts/*.md",
+    [
+      "title: text",
+      "summary: textarea",
+      "tags: list",
+      "image: file",
+      "draft: checkbox",
+      "show_toc: checkbox",
+      "content: markdown",
+    ],
+  )
+  .collection("paxinas", "src:pages", [
+    "title: text",
+    "url: text",
+    "content: markdown",
+  ])
+  .collection("categorías", "src:categories", [
+    "title: text",
+    "id: text",
+    "tags: list",
+  ]);
+
+export default app;
