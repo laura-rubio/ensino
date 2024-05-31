434a65d69d53a684a9c06abbc056eac7bf918ae9
LumeCMS <laura.rubio.rendo@gmail.com>
2024-05-31 17:41:10 +0000

    removed files

--- _cms.lume.ts
+++
@@ -1,7 +0,0 @@
-import site from "./_config.ts";
-import cms from "./_cms.ts";
-import adapter from "lume/cms/adapters/lume.ts";
-
-cms.options.auth = undefined;
-
-export default await adapter({ site, cms });
